da3c2e9a1764d7e5893ce5756d627f6b29d10e72
mariavaccario <mvaccario@udesa.edu.ar>
2022-11-03 17:07:32 -0300

    primer commit, react native

---
+++ src/screens/Home.js
@@ -0,0 +1,22 @@
+import react, {Component} from "react";
+import {View, Text, TouchableOpacity, StyleSheet} from "react-native"
+import { Button } from "react-native-web";
+
+class Home extends Component{
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+
+
+    render(){
+        return(
+            <p>hola</p>
+
+        )
+    }
+}
+
+export default Home;
